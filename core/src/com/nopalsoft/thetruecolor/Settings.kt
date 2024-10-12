8a31bab565a6b1faca4fe468ad2475e2535643b7
hojat <hojat72elect@gmail.com>
2024-10-11 21:26:24 -0400

    Settings.java -> kt

--- core/src/com/nopalsoft/thetruecolor/Settings.kt
+++ core/src/com/nopalsoft/thetruecolor/Settings.kt
@@ -1,36 +1,40 @@
-package com.nopalsoft.thetruecolor;
+package com.nopalsoft.thetruecolor
 
-import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.Preferences;
-import com.nopalsoft.thetruecolor.scene2d.DialogHelpSettings.Languages;
+import com.badlogic.gdx.Gdx
+import com.nopalsoft.thetruecolor.scene2d.DialogHelpSettings.Languages
 
-public class Settings {
+object Settings {
 
-    public static Languages selectedLanguage = Languages.DEFAULT;
+    @JvmField
+    var selectedLanguage = Languages.DEFAULT
+    var bestScore = 0
 
-    public static int bestScore;
-    public static int numTimesPlayed;
+    @JvmField
+    var numTimesPlayed = 0
 
-    private final static Preferences pref = Gdx.app.getPreferences("com.nopalsoft.thetruecolor");
+    private val pref = Gdx.app.getPreferences("com.nopalsoft.thetruecolor")
 
-    public static void save() {
-        pref.putInteger("bestScore", bestScore);
-        pref.putInteger("numVecesJugadas", numTimesPlayed);
-        pref.putString("selectedLanguage", selectedLanguage.toString());
-        pref.flush();
+    @JvmStatic
+    fun save() {
+        pref.putInteger("bestScore", bestScore)
+        pref.putInteger("numVecesJugadas", numTimesPlayed)
+        pref.putString("selectedLanguage", selectedLanguage.toString())
+        pref.flush()
     }
 
-    public static void load() {
-        bestScore = pref.getInteger("bestScore", 0);
-        numTimesPlayed = pref.getInteger("numVecesJugadas", 0);
-        selectedLanguage = Languages.valueOf(pref.getString("selectedLanguage", Languages.DEFAULT.toString()));
+    @JvmStatic
+    fun load() {
+        bestScore = pref.getInteger("bestScore", 0)
+        numTimesPlayed = pref.getInteger("numVecesJugadas", 0)
+        selectedLanguage =
+            Languages.valueOf(pref.getString("selectedLanguage", Languages.DEFAULT.toString()))
     }
 
-    public static void setNewScore(int score) {
+    @JvmStatic
+    fun setNewScore(score: Int) {
         if (score > bestScore) {
-            bestScore = score;
-            save();
+            bestScore = score
+            save()
         }
     }
-
 }
