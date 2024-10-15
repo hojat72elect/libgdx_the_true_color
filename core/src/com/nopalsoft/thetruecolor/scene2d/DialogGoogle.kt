7e1913c9b53a455531aa7847bc679c876544ae44
hojat <hojat72elect@gmail.com>
2024-10-15 02:40:50 -0400

    DialogGoogle.java -> kt

--- core/src/com/nopalsoft/thetruecolor/scene2d/DialogGoogle.kt
+++ core/src/com/nopalsoft/thetruecolor/scene2d/DialogGoogle.kt
@@ -1,65 +1,62 @@
-package com.nopalsoft.thetruecolor.scene2d;
+package com.nopalsoft.thetruecolor.scene2d
 
-import com.badlogic.gdx.graphics.Color;
-import com.badlogic.gdx.scenes.scene2d.InputEvent;
-import com.badlogic.gdx.scenes.scene2d.Stage;
-import com.badlogic.gdx.scenes.scene2d.ui.Label;
-import com.badlogic.gdx.scenes.scene2d.ui.Label.LabelStyle;
-import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
-import com.badlogic.gdx.scenes.scene2d.ui.TextButton.TextButtonStyle;
-import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
-import com.nopalsoft.thetruecolor.Assets;
-import com.nopalsoft.thetruecolor.screens.Screens;
+import com.badlogic.gdx.graphics.Color
+import com.badlogic.gdx.scenes.scene2d.InputEvent
+import com.badlogic.gdx.scenes.scene2d.Stage
+import com.badlogic.gdx.scenes.scene2d.ui.Label
+import com.badlogic.gdx.scenes.scene2d.ui.Label.LabelStyle
+import com.badlogic.gdx.scenes.scene2d.ui.TextButton
+import com.badlogic.gdx.scenes.scene2d.ui.TextButton.TextButtonStyle
+import com.badlogic.gdx.scenes.scene2d.utils.ClickListener
+import com.nopalsoft.thetruecolor.Assets
+import com.nopalsoft.thetruecolor.screens.Screens
 
-public class DialogGoogle extends Dialog {
-    static final float WIDTH = 440;
-    static final float HEIGHT = 250;
+class DialogGoogle(currentScreen: Screens) : Dialog(currentScreen, WIDTH, HEIGHT, 300f) {
 
-    Label lbText;
-    TextButton btGoogleLogin;
+    private val labelText =
+        Label(languages["loginToGoogle"], LabelStyle(Assets.fontSmall, Color.BLACK))
+    private val buttonGoogleLogin =
+        TextButton("", TextButtonStyle(Assets.buttonGoogleText, null, null, Assets.fontSmall))
 
-    public DialogGoogle(Screens currentScreen) {
-        super(currentScreen, WIDTH, HEIGHT, 300);
+    init {
 
-        setCloseButton(210);
+        setCloseButton(210f)
+        labelText.width = width - 20
+        labelText.setFontScale(.75f)
+        labelText.wrap = true
+        labelText.setPosition(width / 2f - labelText.width / 2f, 140f)
 
-        lbText = new Label(languages.get("loginToGoogle"), new LabelStyle(Assets.fontSmall, Color.BLACK));
-        lbText.setWidth(getWidth() - 20);
-        lbText.setFontScale(.75f);
-        lbText.setWrap(true);
-        lbText.setPosition(getWidth() / 2f - lbText.getWidth() / 2f, 140);
+        screen.addPressEffect(buttonGoogleLogin)
+        buttonGoogleLogin.label.setFontScale(.75f)
 
-        btGoogleLogin = new TextButton("", new TextButtonStyle(Assets.buttonGoogleText, null, null, Assets.fontSmall));
-        screen.addPressEffect(btGoogleLogin);
-        btGoogleLogin.getLabel().setFontScale(.75f);
-
-        btGoogleLogin.addListener(new ClickListener() {
-            @Override
-            public void clicked(InputEvent event, float x, float y) {
+        buttonGoogleLogin.addListener(object : ClickListener() {
+            override fun clicked(event: InputEvent, x: Float, y: Float) {
                 if (game.gameServiceHandler.isSignedIn()) {
-                    game.gameServiceHandler.signOut();
+                    game.gameServiceHandler.signOut()
                 } else {
-                    game.gameServiceHandler.signIn();
+                    game.gameServiceHandler.signIn()
                 }
-                hide();
+                hide()
             }
-        });
+        })
 
-        addActor(lbText);
-        addActor(btGoogleLogin);
-
+        addActor(labelText)
+        addActor(buttonGoogleLogin)
     }
 
-    @Override
-    public void show(Stage stage) {
-        super.show(stage);
+    override fun show(stage: Stage) {
+        super.show(stage)
 
-        String textButton = languages.get("login");
-        if (game.gameServiceHandler.isSignedIn())
-            textButton = languages.get("logout");
+        var textButton = languages.get("login")
+        if (game.gameServiceHandler.isSignedIn()) textButton = languages.get("logout")
 
-        btGoogleLogin.setText(textButton);
-        btGoogleLogin.pack();
-        btGoogleLogin.setPosition(getWidth() / 2f - btGoogleLogin.getWidth() / 2f, 35);
+        buttonGoogleLogin.setText(textButton)
+        buttonGoogleLogin.pack()
+        buttonGoogleLogin.setPosition(width / 2f - buttonGoogleLogin.width / 2f, 35f)
+    }
+
+    companion object {
+        const val WIDTH = 440F
+        const val HEIGHT = 250F
     }
 }
